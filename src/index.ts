e8b2b673af24cbc0bd3d4fb8bbe18baf2f330e64
PatrickJS <github@gdi2290.com>
2016-06-04 17:35:10 -0700

    chore: src

---
+++ src/index.ts
@@ -0,0 +1,51 @@
+import { Component, Host }  from '@angular/core';
+import { NgForm, ControlGroup } from '@angular/common';
+
+@Component({
+  selector: 'ac-form-errors',
+  inputs: [
+    'controlPath: control',
+    'errorTypes: errors'
+  ],
+  template: `
+    <span *ngFor="let error of errorMessages">
+      <span>{{ error.message }}</span>
+    </span>
+  `
+})
+export class AcFormErrors {
+  controlPath: string;
+  errorTypes: {[key: string]: any};
+
+
+  constructor(@Host() public formDir: NgForm) {
+
+  }
+
+  get errorMessages(): Array<{type: string, message: string}> {
+    var form: ControlGroup = this.formDir.form;
+    var control = form.find(this.controlPath);
+    var _errors = [];
+    if (control !== undefined && control !== null  && control.touched) {
+      let errors = Object.keys(this.errorTypes);
+      for (let i = 0; i < errors.length; ++i) {
+        let errorType = errors[i];
+        if (control.hasError(errorType)) {
+          _errors.push({
+            type: errorType,
+            message: this.errorTypes[errorType]
+          });
+        }
+      }
+    }
+    return _errors;
+  }
+  set errorMessages(value) {
+  }
+
+}
+
+
+export const ANGULARCLASS_FORM_ERRORS_DIRECTIVES = [
+  AcFormErrors
+];
